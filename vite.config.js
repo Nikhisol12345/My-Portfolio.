1b9b6dee97a4c070df038e13192eda9ed691d9d4
Nikhisol12345 <84311833+Nikhisol12345@users.noreply.github.com>
2023-09-09 11:56:38 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,24 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       external: ['react-tilt'], // Add other external modules if needed
+//     },
+//   },
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-tilt'], // Add other external modules if needed
-    },
-  },
+  build:{
+    outDir: "build"
+  }
 })
-
 
 // vite.config.j
